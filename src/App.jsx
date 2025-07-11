723dc77ed1171518d77ed1443c20a56ba490a134
Rajiya-22 <shaikrajiyasulthana13455@gmail.com>
2025-07-11 13:38:07 +0530

    Third Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
-import Counter from './components/Counter'
+import Card from './components/Card'
+
 
 function App() {
 
   return (
     <div>
-      <Counter />
+      {/* Attributes added in the <Card> tag are considered as props */}
+      {/* Content added inside the opening and closing tags of Card are considered as props.children */}
+      <Card>
+        <h1>Hellos and welcomes</h1>
+      </Card>
     </div>
   )
 }
